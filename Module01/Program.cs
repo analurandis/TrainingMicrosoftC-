336eabc6706fe692841cbb037ac730ab7ef838cc
analurandis <analurandis@hotmail.com>
2024-03-06 22:34:38 -0300

    conclusion module 01

---
+++ Module01/Program.cs
@@ -0,0 +1,21 @@
+﻿// UNIT 03
+
+Console.WriteLine("Hello World");
+
+//console.WriteLine("Hello World");
+//(3,1): error CS0103: The name 'console' does not exist in the current context
+//Erro linha 3 posicao 1
+
+//Console.WriteLine('Hello World');
+//(7,19): error CS1012: Too many characters in character literal
+//Erro linha 7 posicao 19
+
+Console.Write("Congratulations!");
+Console.Write(" ");
+Console.Write("You wrote your first lines of code.");
+
+//UNIT 04
+Console.WriteLine("This is the first line.");
+Console.Write("This is ");
+Console.Write("the second ");
+Console.Write("line.");
